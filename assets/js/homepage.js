1c56cd07052f5cb5c7ac6fb752ce50d0e09699df
treguv <vladislavtregubov00@gmail.com>
2020-10-20 00:09:52 -0700

    added variable request

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-function getUserRepos(){
-    fetch("https://api.github.com/users/octocat/repos");
+function getUserRepos(user ){
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //Make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        })
+    })
 }
 
-getUserRepos();
+getUserRepos("facebook");
